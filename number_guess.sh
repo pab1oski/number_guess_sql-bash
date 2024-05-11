a2a2e1928971bfb0c1f4fe7a7522e4c3a20065e5
pab1oski211 fernandez <pfernandezb211@gmail.com>
2024-05-11 18:56:11 +0000

    feat:database-interaction

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+## Generate a random number
+
+NUMBER=$((1 + RANDOM % 1000))
+
+## Get the username
+echo -e "Enter your username:"
+read USERNAME
+
+##Check if the username is in the database
+QUERY_USERNAME=$($PSQL "")
+##if it is not insert it
+if [[ -z $QUERY_USERNAME ]]
+then
+## Insert it
+else
+  ## Unwrap the query
+
+  echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+echo uess the secret number between 1 and 1000:
+
+## Start guesing
